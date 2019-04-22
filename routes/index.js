33b5c14925d86104926e2d5eafd49bd4e39d7e91
John <j.c.rowell15@gmail.com>
2019-04-22 13:33:09 -0400

    fixing CORS error

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 var express = require('express');
-var router = express.Router();
+var cors = require('cors');
+
+var app = express();
+
+app.use(cors());
+
+// var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+app.get('/', function(req, res, next) {
   res.json([
     {id: 1, username: "somebody"},
     {id: 2, username: "somebody_else"}
   ])
 });
 
-module.exports = router;
+module.exports = app;
